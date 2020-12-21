7a5edafc579025587a38cb0980f2638138d6bf9d
Ricardo Mariano <oi@r3m.me>
2020-12-21 17:00:02 +1100

    Rest of chapter 4

--- src/main/kotlin/r3m/fun2020/Chapter4.kt
+++ src/main/kotlin/r3m/fun2020/Chapter4.kt
@@ -12,32 +12,32 @@ object None : Option<Nothing>()
 // Key assumption in map(f): f always returns something of type B
 fun <A, B> Option<A>.map(f: (A) -> B): Option<B> =
     when (this) {
-      is Some -> Some(f(this.get))
-      is None -> None
+        is Some -> Some(f(this.get))
+        is None -> None
     }
 
 fun <A, B> Option<A>.flatMap(f: (A) -> Option<B>): Option<B> =
     when (this) {
-      is Some -> f(this.get)
-      is None -> None
+        is Some -> f(this.get)
+        is None -> None
     }
 
 fun <A> Option<A>.getOrElse(default: () -> A): A =
     when (this) {
-      is Some -> this.get
-      is None -> default()
+        is Some -> this.get
+        is None -> default()
     }
 
 fun <A> Option<A>.orElse(ob: () -> Option<A>): Option<A> =
     when (this) {
-      is Some -> this
-      is None -> ob()
+        is Some -> this
+        is None -> ob()
     }
 
 fun <A> Option<A>.filter(f: (A) -> Boolean): Option<A> =
     when {
-      this is Some && f(this.get) -> this
-      else -> None
+        this is Some && f(this.get) -> this
+        else -> None
     }
 
 // 4.2
@@ -59,15 +59,81 @@ fun <A, B, C> map2(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
 // 4.4 - This looks like a great candidate to use the List from Chapter 3.
 fun <A> sequence(xs: List<Option<A>>): Option<List<A>> =
     xs.fold<Option<A>, Option<List<A>>>(Some(emptyList<A>())) { acc, option ->
-      acc.flatMap { list -> option.flatMap { a -> Some(list + a) } }
+        acc.flatMap { list -> option.flatMap { a -> Some(list + a) } }
     }
 
 // 4.5
 fun <A, B> traverse(
-  xa: List<A>,
-  f: (A) -> Option<B>
+    xa: List<A>,
+    f: (A) -> Option<B>
 ): Option<List<B>> = xa.fold<A, Option<List<B>>>(Some(emptyList())) { acc, a ->
-  acc.flatMap { list -> f(a).flatMap { b -> Some(list + b) } }
+    acc.flatMap { list -> f(a).flatMap { b -> Some(list + b) } }
 }
 
+sealed class Either<out E, out A>
+data class Left<out E>(val value: E) : Either<E, Nothing>()
+data class Right<out A>(val value: A) : Either<Nothing, A>()
+
+fun <A> catches(a: () -> A): Either<Exception, A> =
+    try {
+        Right(a())
+    } catch (e: Exception) {
+        Left(e)
+    }
+
+// 4.6
+fun <E, A, B> Either<E, A>.map(f: (A) -> B): Either<E, B> =
+    when (this) {
+        is Left -> this
+        is Right -> Right(f(this.value))
+    }
+
+fun <E, A, B> Either<E, A>.flatMap(f: (A) -> Either<E, B>): Either<E, B> =
+    when (this) {
+        is Left -> this
+        is Right -> f(this.value)
+    }
+
+fun <E, A> Either<E, A>.orElse(
+    f: () -> Either<E, A>
+): Either<E, A> = when (this) {
+    is Left -> f()
+    is Right -> this
+}
+
+fun <E, A, B, C> map2(
+    ae: Either<E, A>,
+    be: Either<E, B>,
+    f: (A, B) -> C
+): Either<E, C> = when {
+    ae is Right && be is Right -> Right(f(ae.value, be.value))
+    ae is Left -> ae
+    be is Left -> be
+    else -> throw IllegalStateException("This shouldn't happen")
+}
+
+// 4.7
+// these are close implementation-wise to 4.4 and 4.5
+fun <E, A> sequence(xs: List<Either<E, A>>): Either<E, List<A>> =
+    xs.fold(Right(emptyList<A>()) as Either<E, List<A>>)
+    { acc, either -> acc.flatMap { list -> either.flatMap { a -> Right(list + a) } } }
+
+fun <E, A, B> traverse2(
+    xa: List<A>,
+    f: (A) -> Either<E, B>
+): Either<E, List<B>> = xa.fold(Right(emptyList<B>()) as Either<E, List<B>>)
+{ acc, a -> acc.flatMap { list -> f(a).flatMap { b -> Right(list + b) } } }
+
+// 4.8
+fun <E, A, B, C> map2x(
+    ae: Either<E, A>,
+    be: Either<E, B>,
+    f: (A, B) -> C
+): Either<List<E>, C> = when {
+    ae is Right && be is Right -> Right(f(ae.value, be.value))
+    ae is Left && be is Left -> Left(listOf(ae.value, be.value))
+    ae is Left -> Left(listOf(ae.value))
+    be is Left -> Left(listOf(be.value))
+    else -> throw IllegalStateException("This shouldn't happen")
+}
 
--- src/test/kotlin/r3m/fun2020/Chapter4Test.kt
+++ src/test/kotlin/r3m/fun2020/Chapter4Test.kt
@@ -1,77 +1,133 @@
-@file:Suppress("UnusedEquals", "UNREACHABLE_CODE")
+@file:Suppress("UnusedEquals", "UNREACHABLE_CODE", "UNUSED_ANONYMOUS_PARAMETER", "RemoveExplicitTypeArguments")
 
 package r3m.fun2020
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-class Chapter4Test: FreeSpec({
-  "4.1" - {
-    "map" {
-      Some(1).map { it + 1 } shouldBe Some(2)
-      None.map { "string: $it" } shouldBe None
+class Chapter4Test : FreeSpec({
+    "4.1" - {
+        "map" {
+            Some(1).map { it + 1 } shouldBe Some(2)
+            None.map { "string: $it" } shouldBe None
+        }
+        "flatMap" {
+            Some(1).flatMap { Some(it + 1) } shouldBe Some(2)
+            Some(1).flatMap { None } shouldBe None
+            None.flatMap { Some("") } shouldBe None
+        }
+        "getOrElse" {
+            Some(1).getOrElse { 2 } shouldBe 1
+            None.getOrElse { 1 } shouldBe 1
+        }
+        "orElse" {
+            Some(1).orElse { Some(2) } shouldBe Some(1)
+            None.orElse { Some(1) } shouldBe Some(1)
+        }
+        "filter" {
+            Some(1).filter { it == 1 } shouldBe Some(1)
+            Some(1).filter { it > 1 } shouldBe None
+            None.filter { it == 1 } shouldBe None
+        }
     }
-    "flatMap" {
-      Some(1).flatMap { Some(it + 1) } shouldBe Some(2)
-      Some(1).flatMap { None } shouldBe None
-      None.flatMap { Some("") } shouldBe None
+    "4.2" - {
+        "variance of nothing is none" {
+            variance(emptyList<Double>()) shouldBe None
+        }
+        "variance of a list is a number" {
+            variance(listOf(1.0, 2.0)) shouldBe Some(0.25)
+        }
     }
-    "getOrElse" {
-      Some(1).getOrElse { 2 } shouldBe 1
-      None.getOrElse { 1 } shouldBe 1
+    "4.3" - {
+        "map2 is None if either input is None" {
+            map2(None, Some(1)) { a, b -> "$a$b" } shouldBe None
+            map2(Some(1), None) { a, b -> "$a$b" } shouldBe None
+            map2(None, None) { a, b -> "$a$b" } shouldBe None
+        }
+        "map2 applies f if both inputs are Some" {
+            map2(Some(1), Some(2)) { a, b -> a + b } shouldBe Some(3)
+        }
     }
-    "orElse" {
-      Some(1).orElse { Some(2) } shouldBe Some(1)
-      None.orElse { Some(1) } shouldBe Some(1)
+    "4.4" - {
+        "sequence returns None if any items in the list are None" {
+            sequence(listOf(None)) shouldBe None
+            sequence(listOf(Some(1), Some(2), None)) shouldBe None
+            sequence(listOf(Some(1), None, None, Some(4))) shouldBe None
+        }
+        "sequence returns an option of a list if all items in the list are Some" {
+            sequence(listOf(Some(1))) shouldBe Some(listOf(1))
+            sequence(listOf(Some(1), Some(2), Some(3))) shouldBe Some(listOf(1, 2, 3))
+        }
+        "sequence returns Some(empty) if the list is empty" {
+            sequence(emptyList<Option<Double>>()) shouldBe Some(emptyList<Double>())
+        }
     }
-    "filter" {
-      Some(1).filter { it == 1 } shouldBe Some(1)
-      Some(1).filter { it > 1 } shouldBe None
-      None.filter { it == 1 } shouldBe None
+    "4.5" - {
+        "traverse returns an option of a transformed list if the operation always returns Some" {
+            traverse(listOf(1)) { Some(it + 1) } shouldBe Some(listOf(2))
+            traverse(listOf(1, 2, 3)) { Some(it - 1) } shouldBe Some(listOf(0, 1, 2))
+        }
+        "traverse returns None if the operation returns None on any of the elements" {
+            traverse(listOf(1, 2, 3)) { if (it < 2) Some(it) else None } shouldBe None
+        }
+        "traverse returns Some(empty) if the list is empty" {
+            traverse(emptyList<Int>()) { Some(it) } shouldBe Some(emptyList())
+        }
     }
-  }
-  "4.2" - {
-    "variance of nothing is none" {
-      variance(emptyList<Double>()) shouldBe None
+    "4.6" - {
+        "map" {
+            Left("error").map { "oh $it" } shouldBe Left("error")
+            Right("pass").map { "oh $it" } shouldBe Right("oh pass")
+        }
+        "flatMap" {
+            Left("error").flatMap { Left("oh $it") } shouldBe Left("error")
+            Left("error").flatMap { Right("oh $it") } shouldBe Left("error")
+            Right("pass").flatMap { Left("error") } shouldBe Left("error")
+            Right("pass").flatMap { Right("mountain $it") } shouldBe Right("mountain pass")
+        }
+        "orElse" {
+            Left("error").orElse { Left("more errors") } shouldBe Left("more errors")
+            Left("error").orElse { Right("backup") } shouldBe Right("backup")
+            Right("pass").orElse { Right("backup") } shouldBe Right("pass")
+            Right("pass").orElse { Left("error") } shouldBe Right("pass")
+        }
+        "map2 applies f if both inputs are Right" {
+            map2(Right("a"), Right("b")) { a, b -> "$a$b" } shouldBe Right("ab")
+        }
+        "map2 returns a Left if any of the inputs are Left" {
+            // Well Actually, map2 could do anything it wants, as long as it's a Left.
+            map2(Left("a"), Right("b")) { a, b -> "$a$b" } shouldBe Left("a")
+            map2(Right("a"), Left("b")) { a, b -> "$a$b" } shouldBe Left("b")
+            map2(Left("a"), Left("b")) { a, b -> "$a$b" } shouldBe Left("a")
+        }
     }
-    "variance of a list is a number" {
-      variance(listOf(1.0, 2.0)) shouldBe Some(0.25)
+    "4.7" - {
+        "sequence returns Left if any of the items in the list are left" {
+            sequence(listOf(Left("err"))) shouldBe Left("err")
+            sequence(listOf(
+                Right(1), Right(2), Left("err")
+            )) shouldBe Left("err")
+            sequence(listOf(
+                Right(1), Left("err"), Left("err2"), Right(4)
+            )) shouldBe Left("err")
+        }
+        "sequence returns Right<List> if all of the items in the list are right" {
+            sequence(listOf(Right(1))) shouldBe Right(listOf(1))
+            sequence(listOf(Right(1), Right(2), Right(3))) shouldBe Right(listOf(1, 2, 3))
+        }
+        "sequence returns Right(empty) if the list is empty" {
+            sequence(emptyList<Either<String, Double>>()) shouldBe Right(emptyList<Double>())
+        }
+        // kotlin is unhappy about the two traverses, so i'm calling this one traverse2
+        "traverse returns an Right of a transformed list if the operation always returns Right" {
+            traverse2(listOf(1)) { Right(it + 1) } shouldBe Right(listOf(2))
+            traverse2(listOf(1, 2, 3)) { Right(it - 1) } shouldBe Right(listOf(0, 1, 2))
+        }
+        "traverse returns Left if the operation returns Left on any of the elements" {
+            traverse2(listOf(1, 2, 3)) { if (it < 2) Right(it) else Left("err") } shouldBe Left("err")
+        }
+        "traverse returns Right(empty) if the list is empty" {
+            traverse2(emptyList<Int>()) { Right(it) } shouldBe Right(emptyList())
+        }
     }
-  }
-  "4.3" - {
-    "map2 is None if either input is None" {
-      map2(None, Some(1)) { a, b -> "$a$b" } shouldBe None
-      map2(Some(1), None) { a, b -> "$a$b" } shouldBe None
-      map2(None, None) { a, b -> "$a$b" } shouldBe None
-    }
-    "map2 applies f if both inputs are Some" {
-      map2(Some(1), Some(2)) { a, b -> a + b } shouldBe Some(3)
-    }
-  }
-  "4.4" - {
-    "sequence returns None if any items in the list are None" {
-      sequence(listOf(None)) shouldBe None
-      sequence(listOf(Some(1), Some(2), None)) shouldBe None
-      sequence(listOf(Some(1), None, None, Some(4))) shouldBe None
-    }
-    "sequence returns an option of a list if all items in the list are Some" {
-      sequence(listOf(Some(1))) shouldBe Some(listOf(1))
-      sequence(listOf(Some(1), Some(2), Some(3))) shouldBe Some(listOf(1, 2, 3))
-    }
-    "sequence returns Some(empty) if the list is empty" {
-      sequence(emptyList<Option<Double>>()) shouldBe Some(emptyList<Double>())
-    }
-  }
-  "4.5" - {
-    "traverse returns an option of a transformed list if the operation always returns Some" {
-      traverse(listOf(1)) { Some(it + 1) } shouldBe Some(listOf(2))
-      traverse(listOf(1, 2, 3)) { Some(it - 1) }shouldBe Some(listOf(0, 1, 2))
-    }
-    "traverse returns None if the operation returns None on any of the elements" {
-      traverse(listOf(1, 2, 3)) { if (it < 2) Some(it) else None } shouldBe None
-    }
-    "traverse returns Some(empty) if the list is empty" {
-      traverse(emptyList<Int>()) { Some(it) } shouldBe Some(emptyList())
-    }
-  }
 })
